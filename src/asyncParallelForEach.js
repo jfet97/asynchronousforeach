02637cbf62d2dd6b40a957dd8221cb752752ac99
Edwin Pratt <34472632+Edwin-Pratt@users.noreply.github.com>
2019-03-18 17:41:50 +0200

    Added some spacing and return statement
    I've added some spacing to help with readability. I've also added a return statement.

--- src/asyncParallelForEach.js
+++ src/asyncParallelForEach.js
@@ -1,13 +1,12 @@
 const asyncParallelForEach = Symbol('asyncParallelForEach');
-Array.prototype[asyncParallelForEach] = async function (callback, thisArg) {
 
+Array.prototype[asyncParallelForEach] = async function (callback, thisArg) {
     if (this == null) {
-        // this should be the array on which we perform the async iteration
-        throw new TypeError('this is null or not defined');
+        // This should be the array on which we perform the async iteration
+        throw new TypeError('This array is null or undefined');
     }
 
     const O = Object(this);
-
     const len = O.length >>> 0;
 
     if (typeof callback !== 'function') {
@@ -19,19 +18,25 @@ Array.prototype[asyncParallelForEach] = async function (callback, thisArg) {
         T = thisArg;
     }
 
-    let k = 0;
     let arrayOfReturnedPromises = [];
+    let k = 0;
+    
     while (k < len) {
         let kValue;
+        
         if (k in O) {
             kValue = O[k];
+            
             arrayOfReturnedPromises.push(callback.call(T, kValue, k, O));
         }
+        
         k++;
     }
 
     await Promise.all(arrayOfReturnedPromises);
-    // forEach does return undefined
+    
+    // forEach returns undefined
+    return;
 };
 
 export { asyncParallelForEach };
