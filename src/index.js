01affa94527dfc574f1fa2de956934c210b40817
Edwin Pratt <34472632+Edwin-Pratt@users.noreply.github.com>
2019-03-18 17:45:23 +0200

    Added some spacing
    I've added some spacing to improve readability.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { asyncSerialForEach } from './asyncSerialForEach';
 import { asyncParallelForEach } from './asyncParallelForEach';
+
 export {
     asyncSerialForEach,
     asyncParallelForEach
